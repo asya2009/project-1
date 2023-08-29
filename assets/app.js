ed295afe122a937f2ffe6f4e7f20bf609192dc9f
asya2009 <asya4072@gmail.com>
2023-08-29 18:54:59 +0200

    update

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+const password = "GeheimesPasswort";
+
+if (password = password.length > 8) {
+  console.log(`8 Zeichenlänge ist erfüllt`);
+} else {
+  console.log(`${password} ist nicht möglich!`);
+}
